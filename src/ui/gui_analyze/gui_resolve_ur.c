5bc0ab703b9794303a99f4c49a116e9a44fc2ab0
Adam Tucker <adam@osmosis.team>
2026-06-12 12:14:33 -0600

    Use explicit Zcash batch UR routing

--- src/ui/gui_analyze/gui_resolve_ur.c
+++ src/ui/gui_analyze/gui_resolve_ur.c
@@ -29,7 +29,6 @@ static SetChainData_t g_chainViewArray[] = {
     {REMAPVIEW_BTC_MESSAGE, (SetChainDataFunc)GuiSetPsbtUrData},
 #ifdef CYPHERPUNK_VERSION
     {REMAPVIEW_ZCASH, (SetChainDataFunc)GuiSetZcashUrData},
-    {REMAPVIEW_ZCASH_BATCH_TX, (SetChainDataFunc)GuiSetZcashBatchUrData},
     {REMAPVIEW_XMR_OUTPUT, (SetChainDataFunc)GuiSetMoneroUrData},
     {REMAPVIEW_XMR_UNSIGNED, (SetChainDataFunc)GuiSetMoneroUrData},
 #endif
